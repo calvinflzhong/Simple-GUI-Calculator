f5c62e352c437827bfa1924e99996cce0e917d78
calvinflzhong <calvinfulongzhong@gmail.com>
2022-02-26 18:46:50 -0500

    commit Calculator class

--- src/Calculator.java
+++ src/Calculator.java
@@ -30,36 +30,4 @@ public class Calculator{
         BigDecimal bdB= BigDecimal.valueOf(B);
         return bdA.divide(bdB, 16, RoundingMode.HALF_UP).doubleValue();
     }
-}import java.math.BigDecimal;
-        import java.math.RoundingMode;
-
-public class Calculator{
-
-    //Create method for addition using BigDecimal to avoid double rounding errors
-    public static double add(double A, double B){
-        BigDecimal bdA= BigDecimal.valueOf(A);
-        BigDecimal bdB= BigDecimal.valueOf(B);
-        return bdA.add(bdB).doubleValue();
-    }
-
-    //Create method for subtraction
-    public static double subtract(double A, double B){
-        BigDecimal bdA= BigDecimal.valueOf(A);
-        BigDecimal bdB= BigDecimal.valueOf(B);
-        return bdA.subtract(bdB).doubleValue();
-    }
-
-    //Create method for multiplication
-    public static double multiply(double A, double B){
-        BigDecimal bdA= BigDecimal.valueOf(A);
-        BigDecimal bdB= BigDecimal.valueOf(B);
-        return bdA.multiply(bdB).doubleValue();
-    }
-
-    //Create method for division, set scale as 16(rounding digits)
-    public static double divide(double A, double B){
-        BigDecimal bdA= BigDecimal.valueOf(A);
-        BigDecimal bdB= BigDecimal.valueOf(B);
-        return bdA.divide(bdB, 16, RoundingMode.HALF_UP).doubleValue();
-    }
 }
